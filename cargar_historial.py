e03eb581efcb4ad560dadfc631ad0f8b3df33b1e
owen garcia <owengarciaa1@gmail.com>
2026-06-30 22:08:10 -0600

    editando el historial

--- cargar_historial.py
+++ cargar_historial.py
@@ -24,12 +24,12 @@ def cargar_historial(padre):
         acciones_frame = tk.Frame(datos_frame, bg="#313145")
         acciones_frame.pack(side="right", padx=20)
 
-        tk.Label(datos_frame, text=f"Venta #{venta['num_venta']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(datos_frame, text=f"Cliente: {venta['cliente']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(datos_frame, text=f"Producto: {venta['producto']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(datos_frame, text=f"Cantidad: {venta['cantidad']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(datos_frame, text=f"Precio: ${venta['precio']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(datos_frame, text=f"Total: ${venta['total']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"📦Venta #{venta['num_venta']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
+        tk.Label(datos_frame, text=f" 👤Cliente: {venta['cliente']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
+        tk.Label(datos_frame, text=f"📦 Producto: {venta['producto']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
+        tk.Label(datos_frame, text=f" 🔢Cantidad: {venta['cantidad']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
+        tk.Label(datos_frame, text=f" 💲Precio: ${venta['precio']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
+        tk.Label(datos_frame, text=f" 🟰Total: ${venta['total']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
 
         tk.Button(acciones_frame, text="Editar", bg="#7A68EE", width=10, height=2, font=("arial", 10, "bold"), fg="#fbfbfb", 
         cursor="hand2",).pack(pady=5, padx=10)
