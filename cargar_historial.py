cf63c74c06d59eb2d3d6206b32b2856ac2867b15
Nehemias <nehemiastobar30@gmail.com>
2026-06-30 20:54:25 -0600

    boton editar y eliminar agregados

--- cargar_historial.py
+++ cargar_historial.py
@@ -14,12 +14,24 @@ def cargar_historial(padre):
     ventas_diccionario = compras["Ventas_registradas"]
 
     for venta in ventas_diccionario:
-        venta_frame = tk.Frame(padre, bg="#313145")
-        venta_frame.pack(fill="x", pady=8, padx=18)
+        datos_frame = tk.Frame(padre, bg="#313145")
+        datos_frame.pack(fill="x", pady=8, padx=18)
 
-        tk.Label(venta_frame, text=f"Venta #{venta['num_venta']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(venta_frame, text=f"Cliente: {venta['cliente']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(venta_frame, text=f"Producto: {venta['producto']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(venta_frame, text=f"Cantidad: {venta['cantidad']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(venta_frame, text=f"Precio: ${venta['precio']}", bg="#313145", fg="white").pack(anchor="w")
-        tk.Label(venta_frame, text=f"Total: ${venta['total']}", bg="#313145", fg="white").pack(anchor="w")
+
+        datos_frame = tk.Frame(datos_frame, bg="#313145")
+        datos_frame.pack(side="left", fill="x", expand=True)
+
+        acciones_frame = tk.Frame(datos_frame, bg="#313145")
+        acciones_frame.pack(side="right", padx=20)
+
+        tk.Label(datos_frame, text=f"Venta #{venta['num_venta']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"Cliente: {venta['cliente']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"Producto: {venta['producto']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"Cantidad: {venta['cantidad']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"Precio: ${venta['precio']}", bg="#313145", fg="white").pack(anchor="w")
+        tk.Label(datos_frame, text=f"Total: ${venta['total']}", bg="#313145", fg="white").pack(anchor="w")
+
+        tk.Button(acciones_frame, text="Editar", bg="#7A68EE", width=10, height=2, font=("arial", 10, "bold"), fg="#fbfbfb", 
+        cursor="hand2",).pack(pady=5, padx=10)
+        tk.Button(acciones_frame, text="Eliminar", bg="#7A68EE", width=10, height=2, font=("arial", 10, "bold"), fg="#fbfbfb", 
+        cursor="hand2",).pack(pady=5, padx=10)
