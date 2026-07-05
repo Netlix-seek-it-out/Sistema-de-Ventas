d1504c32e24da61e14057c1359b8a021492b7dd0
Nehemias <nehemiastobar30@gmail.com>
2026-07-05 13:57:33 -0600

    Editar_frame agregado con el boton, falta hacerlo funcionar. Elminar falla

--- cargar_historial.py
+++ cargar_historial.py
@@ -2,7 +2,7 @@ import json
 import tkinter as tk
 from eliminar_venta import eliminar_venta
 
-def cargar_historial(padre):
+def cargar_historial(padre, abrir_edicion):
 
 
 
@@ -32,8 +32,12 @@ def cargar_historial(padre):
         tk.Label(datos_frame, text=f"💲 Precio: ${venta['precio']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
         tk.Label(datos_frame, text=f"🟰 Total: ${venta['total']}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
         tk.Label(datos_frame, text=f"📅 Fecha: {venta.get('fecha', 'Sin fecha')}", bg="#313145", fg="white", font=("Arial", 10, "bold")).pack(anchor="w")
-        tk.Button(acciones_frame, text="Editar", bg="#7A68EE", width=10, height=2, font=("arial", 10, "bold"), fg="#fbfbfb", 
-        cursor="hand2",).pack(pady=5, padx=10)
+
+        tk.Button(acciones_frame, text="Editar",
+            command=lambda venta=venta: abrir_edicion(venta),
+
+            bg="#7A68EE", width=10, height=2, font=("arial", 10, "bold"), fg="#fbfbfb", 
+            cursor="hand2").pack(pady=5, padx=10)
 
         #id = venta["num_venta"]
         tk.Button(
